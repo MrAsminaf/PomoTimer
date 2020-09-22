72b8bbf5e1742a7d6c508b1b0635bf4df6e2fa58
Adam Olgrzymek <mrasminaf@gmail.com>
2020-09-22 10:58:54 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PomoTimer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
