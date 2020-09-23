3468ac9f63f496e762bb1f4070fb76261402237b
Adam Olgrzymek <mrasminaf@gmail.com>
2020-09-23 23:06:46 +0200

    Modified timer code

--- wwwroot/js/timer.js
+++ wwwroot/js/timer.js
@@ -2,7 +2,27 @@ function UpdateTimer() {
     let now = new Date();
     let time = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
 
-    //document.querySelector(".timer").innerHTML = time;
+    document.querySelector(".timer").innerHTML = time;
 }
 
-setInterval(UpdateTimer, 1000);
+function timerOn() {
+    let timerVal = document.querySelector(".timer").innerHTML;
+    console.log(timerVal);
+    isTurnedOn = !isTurnedOn;
+}
+
+function increment() {
+    i = ++i;
+    console.log(i);
+}
+
+let i = 0;
+let isTurnedOn = false;
+
+let int = setInterval(UpdateTimer, 1000);
+
+if (isTurnedOn) {
+    setInterval(increment, 1000);
+} else {
+    clearInterval(int);
+}
